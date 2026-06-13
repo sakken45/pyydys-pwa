c460b317e6dbb39852fb07b0c3e5e070f3f1bf22
sakken45 <sakken45@users.noreply.github.com>
2026-06-13 05:55:23 +0000

    deploy: ca13b81feaefa29b23b5709bee0069a918bb958a ca13b81feaefa29b23b5709bee0069a918bb958a

--- service-worker.js
+++ service-worker.js
@@ -11,7 +11,7 @@
  * register-sw script so users automatically receive updates.
  */
 
-const CACHE_NAME = "pyydys-cache-vmq5idnf2";
+const CACHE_NAME = "pyydys-cache-vmqbxyn4i";
 const APP_SHELL = [
   ".",
   "index.html",
